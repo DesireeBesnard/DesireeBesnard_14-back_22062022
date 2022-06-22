8b11c79670dd28641e9f53deb684dd081540d96a
DesireeBesnard <desireebesnard@gmail.com>
2022-06-22 13:29:42 +0200

    settup server

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyPArser = require('bodyParser')
+
+// Create express app
+const app = express()
+
+//Database
+mongoose.connect('mongodb://localhost/hrnetDB')
+const db = mongoose.connection
+db.once('open', () => {
+    console.log("Connected to MongoDB database")
+})
+
+//Routes
+app.get('/', (req, res) => {
+    res.send("Hello world!")
+})
+
+
+// Starting server
+app.listen(3001, console.log("Listening on port 3001"))
